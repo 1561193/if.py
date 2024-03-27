288a8f994264b221c245d48ef5151407bb702682
1561193 <164223652+1561193@users.noreply.github.com>
2024-03-27 21:42:17 +0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,26 @@
+x = 36
+print('Привет!')
+if x > 0:
+    print('Верно')
+if x < 0:
+    print('Нет')
+print('Пока')
+a, b = 12, 4
+if a > b:
+    print('a > b')
+if a > b and a > 1:
+    print('Конечно')
+if (a > b) and (a > 0 or b < 100):
+    print('Да')
+if 4 < b and b < 10:
+    print('Да')
+if '45' > '125':
+    print('Верно')
+if '145' > '14':
+    print('Вероятно')
+if [1, 4] > [1, 1]:
+    print('Точно')
+
+
+
+
